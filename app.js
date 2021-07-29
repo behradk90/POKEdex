5eb6933392906c3d23c59190f7e3743af2779305
behradk90 <83545119+behradk90@users.noreply.github.com>
2021-07-29 13:26:40 +0100

    Skills bar added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const container = document.getElementById('container');
 const input = document.querySelector('#input');
 const button = document.querySelector('#submit');
 
+
 button.addEventListener('click', (e) => {
     e.preventDefault();
     getPokemonData(input.value);
@@ -19,7 +20,18 @@ const getPokemonData = async (query) => {
 
     const pokemon = await res.json()
     console.log(pokemon)
+
     document.getElementById('img').setAttribute('src', pokemon.sprites.other.dream_world.front_default)
-    document.getElementById('caption').innerText = pokemon.name
+    document.getElementById('caption').innerText = pokemon.name;
+    document.getElementById('type').innerText = `Type: ${pokemon.types.map((type) => type.type.name).join(' / ')}`
+    document.getElementById('id').innerText = `# ${pokemon.id.toLocaleString('en-US', { minimumIntegerDigits: 3, useGrouping: false })}`
+    document.getElementById('HP').innerText = `${pokemon.stats[0].base_stat}/255`
+    document.getElementById('attack').innerText = `${pokemon.stats[1].base_stat}/255`
+    document.getElementById('defence').innerText = `${pokemon.stats[2].base_stat}/255`
+    document.getElementById('special-attack').innerText = `${pokemon.stats[3].base_stat}/255`
+    document.getElementById('special-defence').innerText = `${pokemon.stats[4].base_stat}/255`
+    document.getElementById('speed').innerText = `${pokemon.stats[5].base_stat}/255`
+    input.value = ""
 }
 
+// getPokemonData(6)
