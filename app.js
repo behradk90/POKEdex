d9c7180e214d861dbea342a115b93536db42d608
behradk90 <83545119+behradk90@users.noreply.github.com>
2021-08-12 17:40:35 +0100

    Evolution chain display achieved

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const indexContainer = document.querySelector('.index-container');
 const regionContainer = document.querySelector('.region-container');
 const errorContainer = document.getElementById('errorContainer')
 const header = document.querySelector('h1')
+const evoContainer = document.getElementById('evolutionChain')
 
 const input = document.querySelector('#input');
 const button = document.querySelector('#submit');
@@ -17,6 +18,15 @@ const defenceStat = document.querySelector('.defence-stat');
 const specialAttackStat = document.querySelector('.special-attack-stat');
 const specialDefenceStat = document.querySelector('.special-defence-stat');
 const speedStat = document.querySelector('.speed-stat');
+
+const kanto = document.querySelector('.kanto')
+const johto = document.querySelector('.johto')
+const hoenn = document.querySelector('.hoenn')
+const sinnoh = document.querySelector('.sinnoh')
+const unova = document.querySelector('.unova')
+const kalos = document.querySelector('.kalos')
+const alola = document.querySelector('.alola')
+const galar = document.querySelector('.galar')
 
 header.addEventListener('click', () => {
     indexContainer.innerHTML = ""
@@ -30,44 +40,15 @@ header.addEventListener('click', () => {
 button.addEventListener('click', (e) => {
     e.preventDefault();
     getPokemonData(input.value.toLowerCase());
+    morePokemonData(input.value.toLowerCase())
 })
 input.addEventListener("keypress", function onEvent(e) {
     if (e.key === "Enter") {
         e.preventDefault();
         getPokemonData(input.value.toLowerCase());
+        morePokemonData(input.value.toLowerCase())
     }
 });
-
-function indexCard(indexStart, indexEnd) {
-    for (let i = indexStart; i <= indexEnd; i++) {
-        const pokemon = document.createElement('div');
-        const label = document.createElement('span');
-        label.innerText = `# ${i.toLocaleString('en-US', { minimumIntegerDigits: 3, useGrouping: false })}`
-        const newImg = document.createElement('img');
-        newImg.src = `${spritesUrl}${i}.png`;
-        pokemon.appendChild(newImg);
-        pokemon.appendChild(label);
-        indexContainer.appendChild(pokemon);
-        pokemon.addEventListener('click', () => {
-            getPokemonData(i)
-            morePokemonData(i)
-            pokemonDetailsContainer.style.display = "block"
-            regionContainer.style.display = "none"
-        })
-    }
-}
-
-
-indexCard(1, 151)
-document.querySelector('.kanto').addEventListener('click', () => { indexContainer.innerHTML = "", indexCard(1, 151) })
-document.querySelector('.johto').addEventListener('click', () => { indexContainer.innerHTML = "", indexCard(152, 251) })
-document.querySelector('.hoenn').addEventListener('click', () => { indexContainer.innerHTML = "", indexCard(252, 386) })
-document.querySelector('.sinnoh').addEventListener('click', () => { indexContainer.innerHTML = "", indexCard(387, 493) })
-document.querySelector('.unova').addEventListener('click', () => { indexContainer.innerHTML = "", indexCard(494, 649) })
-document.querySelector('.kalos').addEventListener('click', () => { indexContainer.innerHTML = "", indexCard(650, 721) })
-document.querySelector('.alola').addEventListener('click', () => { indexContainer.innerHTML = "", indexCard(722, 809) })
-document.querySelector('.galar').addEventListener('click', () => { indexContainer.innerHTML = "", indexCard(810, 898) })
-
 
 const getPokemonData = async (query) => {
     const res = await fetch(`${url}pokemon/${query}`);
@@ -95,8 +76,7 @@ const getPokemonData = async (query) => {
     document.getElementById('defence').innerText = `${pokemon.stats[2].base_stat}/255`
     document.getElementById('specialAttack').innerText = `${pokemon.stats[3].base_stat}/255`
     document.getElementById('specialDefence').innerText = `${pokemon.stats[4].base_stat}/255`
-    document.getElementById('speed').innerText = `${pokemon.stats[5].base_stat}/255`
-    document.getElementById('')
+
     HPStat.value = pokemon.stats[0].base_stat
     attackStat.value = pokemon.stats[1].base_stat
     defenceStat.value = pokemon.stats[2].base_stat
@@ -104,13 +84,46 @@ const getPokemonData = async (query) => {
     specialDefenceStat.value = pokemon.stats[4].base_stat
     speedStat.value = pokemon.stats[5].base_stat
     input.value = ""
-    // console.log(pokemon)
+    console.log(pokemon)
 }
+
+function indexCard(indexStart, indexEnd) {
+    for (let i = indexStart; i <= indexEnd; i++) {
+        const pokemon = document.createElement('div');
+        const label = document.createElement('span');
+        label.innerText = `# ${i.toLocaleString('en-US', { minimumIntegerDigits: 3, useGrouping: false })}`
+        const newImg = document.createElement('img');
+        newImg.src = `${spritesUrl}${i}.png`;
+        pokemon.appendChild(newImg);
+        pokemon.appendChild(label);
+        indexContainer.appendChild(pokemon);
+        pokemon.addEventListener('click', () => {
+            getPokemonData(i)
+            morePokemonData(i)
+            pokemonDetailsContainer.style.display = "block"
+            regionContainer.style.display = "none"
+        })
+    }
+}
+
+
+indexCard(1, 151)
+kanto.addEventListener('click', () => { indexContainer.innerHTML = "", indexCard(1, 151) })
+johto.addEventListener('click', () => { indexContainer.innerHTML = "", indexCard(152, 251) })
+hoenn.addEventListener('click', () => { indexContainer.innerHTML = "", indexCard(252, 386) })
+sinnoh.addEventListener('click', () => { indexContainer.innerHTML = "", indexCard(387, 493) })
+unova.addEventListener('click', () => { indexContainer.innerHTML = "", indexCard(494, 649) })
+kalos.addEventListener('click', () => { indexContainer.innerHTML = "", indexCard(650, 721) })
+alola.addEventListener('click', () => { indexContainer.innerHTML = "", indexCard(722, 809) })
+galar.addEventListener('click', () => { indexContainer.innerHTML = "", indexCard(810, 898) })
+
+
+
 
 const morePokemonData = async (query) => {
     const res = await fetch(`${url}pokemon-species/${query}`)
-    const evolution = await res.json()
-    const nextRes = await fetch(`${evolution.evolution_chain.url}`)
+    const pokemonDetails = await res.json()
+    const nextRes = await fetch(`${pokemonDetails.evolution_chain.url}`)
     const data = await nextRes.json()
     console.log(data.chain)
 
@@ -121,27 +134,42 @@ const morePokemonData = async (query) => {
         let numberOfEvolutions = evoData['evolves_to'].length;
 
         evoChain.push({
-            "species_name": evoData.species.name[0].toUpperCase() + evoData.species.name.slice(1)
+            "species_name": evoData.species.name[0].toUpperCase() + evoData.species.name.slice(1),
+            "pokemon_id": evoData.species.url[0] + evoData.species.url.slice(1)
         });
 
         if (numberOfEvolutions > 1) {
             for (let i = 1; i < numberOfEvolutions; i++) {
                 evoChain.push({
-                    "species_name": evoData.species.name[0].toUpperCase() + evoData.species.name.slice(1)
+                    "species_name": evoData.species.name[0].toUpperCase() + evoData.species.name.slice(1),
+                    "pokemon_id": evoData.species.url[0] + evoData.species.url.slice(1)
                 });
             }
         }
 
         evoData = evoData.evolves_to[0];
-
+        evoContainer.innerHTML = ""
     } while (!!evoData && evoData.hasOwnProperty('evolves_to'));
     for (let i = 0; i < evoChain.length; i++) {
         if (evoChain.length <= 1) {
-            document.getElementById('evolutionInfo').innerText = 'This Pokemon does not evolve'
+            document.getElementById('evolutionChain').innerText = 'This Pokemon does not evolve'
         } else {
-            document.getElementById('evolutionInfo').innerText = evoChain.map((species) => species.species_name).join(' / ')
+            const pokemon = document.createElement('div');
+            const label = document.createElement('span');
+            label.innerText = evoChain.map((species) => species.species_name)[i]
+            const newImg = document.createElement('img');
+            newImg.src = `${spritesUrl}${evoChain[i].pokemon_id.split("/")[6]}.png`;
+            pokemon.appendChild(newImg);
+            pokemon.appendChild(label);
+            evoContainer.appendChild(pokemon)
+            // document.getElementById('evolutionInfo').innerText = evoChain.map((species) => species.species_name).join(' / ')
+            pokemon.addEventListener('click', () => {
+                getPokemonData(evoChain[i].pokemon_id.split("/")[6])
+                morePokemonData(evoChain[i].pokemon_id.split("/")[6])
+                pokemonDetailsContainer.style.display = "block"
+                regionContainer.style.display = "none"
+            })
         }
-        console.log(evoChain[i].species_name)
     }
 }
 
